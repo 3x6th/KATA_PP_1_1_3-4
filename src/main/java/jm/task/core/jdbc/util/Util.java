64ec0bb04416d12ebeb037445a05598d34412675
smashbuster <smashbaster@gmail.com>
2022-09-25 13:00:19 +0700

    class Util is done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final String URL = "jdbc:mysql://localhost:3306/my_base";
+    private final String USER = "root";
+    private final String PASSWORD = "!@QWaszx12";
+
+    public Connection connection() {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            return connection;
+        } catch (SQLException e) {
+            System.err.println("Не удалось загурзить");
+            throw new RuntimeException(e);
+        }
+    }
+
 }
