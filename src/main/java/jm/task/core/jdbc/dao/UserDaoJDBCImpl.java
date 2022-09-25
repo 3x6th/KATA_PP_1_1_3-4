7738067827749210ca1482e9819af7f830a86904
smashbuster <smashbaster@gmail.com>
2022-09-25 21:02:03 +0700

    UsserDaoJDBCImpl has two methods

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,19 +2,50 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getConnection;
+
 public class UserDaoJDBCImpl implements UserDao {
+    private final Connection connection = getConnection();
+    private PreparedStatement preparedStatement = null;
+    private String query = null;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
 
+        query = "CREATE TABLE `my_base`.`users` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NULL,\n" +
+                "  `lastName` VARCHAR(45) NULL,\n" +
+                "  `age` INT NULL,\n" +
+                "  PRIMARY KEY (`id`))";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Не удалось загурзить");
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        query = "DROP TABLE `my_base`.`users`";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Не удалось удалить");
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
