ddc422d3c2010cd72ba07f0e9125228865f529e2
smashbuster <smashbaster@gmail.com>
2022-09-30 00:04:35 +0700

    PAIN

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getSessionFactory;
+
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,7 +19,21 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (\n" +
+                            "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                            "  name VARCHAR(45) NULL,\n" +
+                            "  lastName VARCHAR(45) NULL,\n" +
+                            "  age INT NULL,\n" +
+                            "  PRIMARY KEY (id))")
+                    .addEntity(User.class);
+            query.executeUpdate();
+        } catch (Exception e) {
+            assert transaction != null;
+            transaction.rollback();
+        }
     }
 
     @Override
